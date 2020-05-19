a2ce06d513eb6f1bee90ac5a01b85ff7e99ecad1
Santo Commarato <cylynex@gmail.com>
2020-05-19 11:44:57 -0600

    New Obstacle Controller - Fader (In/Out including collider)

---
+++ Assets/Scripts/Obstacles/Fading.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fading : MonoBehaviour {
+
+    [SerializeField] float adjustAmount = 1f;
+    Color objectColor;
+    bool direction = false;
+    float safeTimer = 5f;
+    float timer = 0;
+    bool positionTimer;
+
+    private void Start() {
+        objectColor = GetComponent<MeshRenderer>().material.color;
+    }
+
+    private void FixedUpdate() {
+
+        if (positionTimer) {
+            timer -= Time.deltaTime;
+            if (timer <= 0) {
+                positionTimer = false;
+                GetComponent<BoxCollider>().enabled = true;
+            }
+        } else {
+
+            if (direction == false) {
+                AlphaDown();
+            } else {
+                AlphaUp();
+            }
+
+            GetComponent<MeshRenderer>().material.color = objectColor;
+
+            if (objectColor.a <= 0) {
+                direction = true;
+                timer = safeTimer;
+                positionTimer = true;
+                GetComponent<BoxCollider>().enabled = false;
+            } else if (objectColor.a >= 1) {
+                direction = false;
+            }
+        }
+    }   
+
+    void AlphaDown() {
+        objectColor.a -= adjustAmount * Time.deltaTime;
+    }
+
+    void AlphaUp() {
+        objectColor.a += adjustAmount * Time.deltaTime;
+    }
+
+}
