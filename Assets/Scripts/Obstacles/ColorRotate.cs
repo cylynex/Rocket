ba9af93e6ea700dc9d2424be1ce8258d6f929c6c
Santo Commarato <cylynex@gmail.com>
2020-05-19 09:52:02 -0600

    New Obstacle Script - color changer (demo2 updated)

---
+++ Assets/Scripts/Obstacles/ColorRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorRotate : MonoBehaviour {
+
+    [SerializeField] Color color1;
+    [SerializeField] Color color2;
+    [SerializeField] Color currentColor;
+    [SerializeField] float speedFactor = 1f;
+    Color nextColor;
+    float timer;
+
+    private void Start() {
+        currentColor = color1;
+        nextColor = color2;
+    }
+
+    private void Update() {
+        if (currentColor == color1) {
+            nextColor = color2;
+        } else if (currentColor == color2) {
+            nextColor = color1;
+        }
+
+        timer = 0;
+        timer += Time.deltaTime * speedFactor;
+        currentColor = Color.Lerp(currentColor, nextColor, timer);
+        GetComponent<MeshRenderer>().material.color = currentColor;
+    }
+
+}
---
+++ Assets/Scripts/Obstacles/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    [SerializeField] float rotateSpeed;
+    [SerializeField] Vector3 rotateVector;
+
+    private void Update() {
+        transform.Rotate(rotateVector * Time.deltaTime);
+    }
+
+}
