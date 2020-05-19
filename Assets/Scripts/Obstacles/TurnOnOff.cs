ff69c9295172718850ae19b982e32ba494a4ffd4
Santo Commarato <cylynex@gmail.com>
2020-05-19 10:00:09 -0600

    Obstacle Controllers Added

---
+++ Assets/Scripts/Obstacles/TurnOnOff.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnOff : MonoBehaviour {
+
+    [SerializeField] GameObject blinkItem;
+    [SerializeField] float activationTimer;
+    [SerializeField] float timer;
+    bool onOff = true;
+
+    private void Start() {
+        timer = activationTimer;
+    }
+
+    private void Update() {
+        timer -= Time.deltaTime;
+        if (timer <= 0) {
+            SwapState();
+        }
+    }
+
+    void SwapState() {
+        if (onOff) {
+            print("turn off");
+            blinkItem.SetActive(false);
+            onOff = false;
+        } else {
+            print("turn on");
+            blinkItem.SetActive(true);
+            onOff = true;
+        }
+
+        timer = activationTimer;
+
+    }
+
+}
